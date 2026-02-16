b28be133907d9bacfcaade4326a780d39dbb188f
henrydall@softwarestrategies.io <henrydall@softwarestrategies.io>
2026-02-16 10:16:27 -0800

    Rename things

---
+++ scheduledevent-orchestrator/src/main/java/io/softwarestrategies/scheduledevent/ScheduledeventOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package io.softwarestrategies.scheduledevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduledeventOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduledeventOrchestratorApplication.class, args);
+	}
+
+}
